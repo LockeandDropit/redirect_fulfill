483ccdc80f4b9aae7e2899e25d4f6973a4bc869c
Sven Olafsen <stockburner9@gmail.com>
2023-07-26 14:03:36 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+
+  return (
+    <>
+      
+      <h1>You're all set!</h1>
+      <div className="card">
+      <a href="fulfill://OnboardingComplete">
+          Back to app
+        </a>
+        
+      </div>
+     
+    </>
+  )
+}
+
+export default App
